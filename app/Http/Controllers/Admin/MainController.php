70db1f7381b44bca8c015796f6f772700e98e68e
Pavel <pawelkorepanov105@gmail.com>
2026-05-21 22:49:46 +0500

    task4

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -9,6 +9,9 @@ class MainController extends Controller
 {
     public function index() 
     {
+        $tag = new Tag();
+        $tag->title = 'Привет мир!';
+        $tag->save();
         return view('admin.index');
     }
 }
---
+++ app/Models/Tag.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+
+class Tag extends Model
+{
+    use Sluggable;
+
+    public function posts() 
+    {
+        return $this->belongsToMany(Post::class);
+    }
+
+    public function sluggable() {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+}
